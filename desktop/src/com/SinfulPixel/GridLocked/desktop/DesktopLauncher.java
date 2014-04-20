64d768ef3ede417cb06958f8571e3d2cdf09fe81
Min3CraftDud3 <hubeb93@gmail.com>
2014-04-20 18:34:05 -0400

    Initial commit

---
+++ desktop/src/com/SinfulPixel/GridLocked/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.SinfulPixel.GridLocked.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.SinfulPixel.GridLocked.GridLocked;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GridLocked(), config);
+	}
+}
